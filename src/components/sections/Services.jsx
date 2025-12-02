2cdeecfb36b723e396c2649022313c0b467b4e70
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-12-02 23:30:10 +0000

    fix: updated ServiceCard with padding on the columns

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -5,7 +5,7 @@ import services from '../../content/services'
 const Services = () => {
     return (
         <section className="w-full h-auto text-center py-24 bg-gray-light">
-            <h2 className="text-2xl md:text-3xl font-bold tracking-tight leading-tight uppercase mb-24">I help businesses elevate their brands with strong visuals and modern web experiences.</h2>
+            <h2 className="text-2xl md:text-3xl font-bold tracking-tight leading-tight uppercase mb-12">I help businesses elevate their brands with strong visuals and modern web experiences.</h2>
             <div className="flex flex-col lg:flex-row gap-x-10">
                 {services.map(({ title, description }) => (
                     <ServiceCard key={title} title={title} description={description} />
--- src/components/ui/ServiceCard.jsx
+++ src/components/ui/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCard = ({ title, description }) => {
     return (
-        <div className="flex-1 my-8 group mx-auto">
+        <div className="flex-1 my-8 group mx-auto px-4 lg:px-8">
             <h3 className="text-2xl text-black-rich font-bold uppercase transition-transform duration-700 ease-out group-hover:scale-105">{title}</h3>
             <p className="text-base text-gray-dark transition-transform duration-700 ease-out group-hover:scale-105">{description}</p>
         </div>
