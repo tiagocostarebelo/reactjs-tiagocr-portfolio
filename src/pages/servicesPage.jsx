ac17f94e26cf6d8617a52de6a9ed30ac6eeb10e3
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-12-08 18:50:00 +0000

    feat: initiated services Page

--- src/pages/servicesPage.jsx
+++ src/pages/servicesPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SectionsHero from '../components/sections/SectionsHero'
 
 const ServicesPage = () => {
     return (
-        <div>servicesPage</div>
+        <>
+            <SectionsHero
+                heading="What can I do for you?"
+                text="I help brands, creators, and small businesses stand out through strong visuals and clean, modern digital experiences."
+                text2="Whether you need a brand, a website, I can help."
+                buttonLabel=""
+            />
+        </>
     )
 }
 
