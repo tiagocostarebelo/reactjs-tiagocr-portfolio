597c1d3fa2a2d9615fb5543c386e6624466b7cef
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2026-03-17 11:11:25 +0000

    feat: update contact style. add new card variation to index.css

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -3,22 +3,22 @@ import Button from '../ui/Button'
 import FadeIn from '../animation/FadeIn'
 
 const Contact = ({
-    heading = "Let's build something great!",
-    text = "Whether you need help with your brand, a website, or support on a digital project. I’m always open to conversations.",
-    buttonLabel = "Get in Touch",
-    bg = "bg-gray-light"
+    heading = "Build a Brand and Website that do your business justice",
+    text = "",
+    buttonLabel = "Start a Project",
+    bg = "bg-mustard"
 }) => {
 
     return (
-        <section className={`w-full h-auto py-24 ${bg} text-center`}>
-            <FadeIn>
-                <h2 className="text-2xl md:text-3xl font-bold text-black-rich tracking-tight leading-tight uppercase mb-6">{heading}</h2>
-                <p className="text-base text-gray-dark">{text}</p>
+        <section id="contact" className={`flex flex-col lg:flex-row justify-between items-center gap-20 py-24 ${bg}`}>
+            <FadeIn delay={120}>
+                <h2 className={`font-display font-bold text-2xl md:text-3xl lg:text-4xl uppercase tracking-tight leading-none text-black-rich max-w-5xl`}>{heading}</h2>
+                <p className={`text-base text-gray-dark`}>{text}</p>
             </FadeIn>
 
-            <div className="mt-12 md:mt-14">
+            <div className="flex-shrink-0">
                 <FadeIn delay={180}>
-                    <Button as="link" to="/contact" className="btn-secondary">{buttonLabel}</Button>
+                    <Button as="link" to="/contact" className="btn-black-rich">{buttonLabel}</Button>
                 </FadeIn>
             </div>
         </section>
